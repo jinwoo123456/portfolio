84c5842869e5f27340e3e167794a39e328d58427
Your <rlawlsdn1020@gmail.com>
2024-05-08 14:30:47 +0900

    포트폴리오0508

---
+++ js/main1.js
@@ -0,0 +1,17 @@
+const character = document.querySelector(".character");
+const rectengle = document.querySelector(".rectengle");
+let degree = 0;
+loop();
+
+function loop() {
+  const rotation = (degree * Math.PI) / 180;
+  const targetX = window.innerWidth / 2 + 80 * Math.cos(rotation) - 10;
+  const targetY = window.innerHeight / 2 + 80 * Math.sin(rotation) - 10;
+
+  character.style.left = `${targetX}px`;
+  character.style.top = `${targetY}px`;
+
+  degree += 1;
+
+  requestAnimationFrame(loop);
+}
